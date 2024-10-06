3b8e11343676b58f958777c65f1a76e7875c8dc8
apenasDaniel <apenasdanieloliveira@gmail.com>
2024-10-07 00:30:20 +0100

    Initial commit: spring boot project with basic configuration

---
+++ src/main/java/com/danieloliveira/quick_wallet/QuickWalletApplication.java
@@ -0,0 +1,13 @@
+package com.danieloliveira.quick_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danieloliveira/quick_wallet/QuickWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieloliveira.quick_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
